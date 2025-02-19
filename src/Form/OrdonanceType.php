11ec2f6952ebffdfee7166d3e48adbae7de33e3d
aridhiseif <80281867+aridhiseif@users.noreply.github.com>
2025-02-19 21:03:07 +0100

    template integration for ordonance

--- src/Form/OrdonanceType.php
+++ src/Form/OrdonanceType.php
@@ -9,6 +9,9 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
 
 class OrdonanceType extends AbstractType
 {
@@ -29,9 +32,13 @@ class OrdonanceType extends AbstractType
             ])
             ->add('medicaments', EntityType::class, [
                 'class' => Medicament::class,
-                'choice_label' => 'id',
-                'multiple' => true,
+                'choice_label' => 'name', // Adjust this based on the Medicament entity field
+                'multiple' => true, // Allow multiple selections
+                'expanded' => true, // Use a dropdown (set to true for checkboxes)
+                'by_reference' => false,
+                'attr' => ['class' => 'box']
             ])
+            ->add('save',SubmitType::class)
         ;
     }
 
